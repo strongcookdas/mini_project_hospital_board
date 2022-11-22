e757297ec22b0ec66fc205b1a3a329e5ff3f1fae
strongcookdas <goodjav141592@gmail.com>
2022-11-22 14:07:03 +0900

    build : start mini_project_hospital_board

---
+++ src/main/java/com/bospital/board/mini_project_hospita_board/MiniProjectHospitaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.bospital.board.mini_project_hospita_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectHospitaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectHospitaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bospital/board/mini_project_hospita_board/MiniProjectHospitaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bospital.board.mini_project_hospita_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectHospitaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
